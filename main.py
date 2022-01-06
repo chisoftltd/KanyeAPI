5d42acc957e204f19c705f4e873c848be75c3b8c
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-06 21:42:31 +0000

    playing with my boys name....

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ def get_quote():
 
 
 window = Tk()
-window.title("Shepherd Quotes ...")
+window.title("Emmanuel Quotes ...")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=300, height=414)
+canvas = Canvas(width=400, height=514, bg="blue")
 bg_img = PhotoImage(file="images/background.png")
-canvas.create_image(150, 207, image=bg_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
+canvas.create_image(200, 257, image=bg_img)
+quote_text = canvas.create_text(200, 257, text="Emmanuel Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
                                 fill="white")
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-kanye_img = PhotoImage(file="images/shepherd.png")
+kanye_img = PhotoImage(file="images/emmanuel.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
+
+she_img = PhotoImage(file="images/shepherd.png")
+she_button = Button(image=she_img, highlightthickness=0, command=get_quote)
+she_button.grid(row=1, column=1)
 
 window.mainloop()
